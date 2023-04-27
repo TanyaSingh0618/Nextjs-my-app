e363ac432b398ed5bf8daca26d8ff121dc3f741a
Admin <admin@Tanyas-MacBook-Air.local>
2023-04-27 14:18:08 +0530

    nextjs project

---
+++ src/app/Blog/[...Slug]/page.tsx
@@ -0,0 +1,6 @@
+
+
+export default function Slug() {
+
+    return <div>blogger</div>
+}
---
+++ src/app/Blog/page.tsx
@@ -0,0 +1,7 @@
+import styles from '../page.module.css'
+
+
+export default function Blog() {
+
+    return <div className={styles.main} >blog</div>
+}
